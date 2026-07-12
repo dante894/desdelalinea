431747b4896aafbd881367614f4814efb2e7e1c1
DanteTrb <trabalon.dante@gmail.com>
2026-07-12 01:47:16 -0300

    Update Database.php

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -10,46 +10,19 @@ class Database
 
     public static function getInstance(): PDO
     {
-        if(self::$instance === null){
-
-<<<<<<< HEAD
+        if (self::$instance === null) {
             $port = $_ENV['DB_PORT'] ?? '3306';
 
-=======
->>>>>>> cb116038913c643d0c8c68dd276ebb93c78d7470
-            self::$instance =
-            new PDO(
-
-                "mysql:host="
-                .$_ENV['DB_HOST'].
-
-<<<<<<< HEAD
-                ";port="
-                .$port.
-
-=======
->>>>>>> cb116038913c643d0c8c68dd276ebb93c78d7470
-                ";dbname="
-                .$_ENV['DB_NAME'].
-
+            self::$instance = new PDO(
+                "mysql:host=" . $_ENV['DB_HOST'] .
+                ";port=" . $port .
+                ";dbname=" . $_ENV['DB_NAME'] .
                 ";charset=utf8mb4",
-
                 $_ENV['DB_USER'],
-
-<<<<<<< HEAD
                 $_ENV['DB_PASS'],
-
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 ]
-=======
-                $_ENV['DB_PASS']
->>>>>>> cb116038913c643d0c8c68dd276ebb93c78d7470
-            );
-
-            self::$instance->setAttribute(
-                PDO::ATTR_ERRMODE,
-                PDO::ERRMODE_EXCEPTION
             );
         }
 
